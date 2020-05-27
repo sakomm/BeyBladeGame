8f00f66d39c2654a96bace8b71e53127aa7161bb
sakomm <sakommuri@gmail.com>
2020-05-27 14:41:51 -0400

    rotation added but only works with 90 degrees, figure out and fix
    maybe another rotate func?

--- TestField.py
+++ TestField.py
@@ -8,11 +8,14 @@ pygame.init() # isnt inatilizing secondary modules GARBAGE SHIT FACE CODE
 
 
 screen = pygame.display.set_mode((1000, 1000))
+pygame.display.set_caption('BeyBlade Game')
 
 # image needs to be in the main file struct cant store in seprate folder, see if u can fix
 #Background obj
 Background = pygame.image.load('ArenaBeyBladeVroom.jpg').convert()
 screen.blit(Background,(0,0))
+
+    #load method can have a specfied path - take that stack overflow u dumb bitch
 
 #player obj - Pegasus
 player = pygame.image.load('PegasusIsLame.jpg') # i think i spelled Pegasus wrong
@@ -22,8 +25,8 @@ pygame.display.update()
 #https://www.pygame.org/docs/ref/transform.html#pygame.transform.rotate
 
 # player2 obj - El Drago
-player = pygame.image.load('ELDrago.png') # i think i spelled Pegasus wrong
-screen.blit(player, (650,650))
+player2 = pygame.image.load('ELDrago.png') # i think i spelled Pegasus wrong
+screen.blit(player2, (650,650))
 pygame.display.update()
 
 done = False
@@ -37,6 +40,14 @@ while not done:
             # update probaby needs to happen hear in loop
 
         #pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(500, 500, 100, 100))
+
+        player = pygame.transform.rotate(player,90)
+        screen.blit(player, (100,100))
+
+
+        player2 = pygame.transform.rotate(player2,90)
+        screen.blit(player2, (650,650))
+
         pygame.display.flip()
 
 # for movement we cant use random values but we can [A,B,C,D] and change the rates at which each grow and decrease to
