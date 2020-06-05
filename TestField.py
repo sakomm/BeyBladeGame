f74ab05f0a15c5f42548ef0d115ccb12e1dd40b8
persepolis7 <rezakhoshhal6@gmail.com>
2020-06-05 15:08:02 -0400

    new

--- TestField.py
+++ TestField.py
@@ -1,73 +1,96 @@
 """ Atom dosent have a # DEBUG: so im fucking dumb but apparently VSC still has a collobration
  tool so might use that
 """
-import pygame #from pygame.locals import * # WTF am i doing here, where the fuck is locals
+import pygame  # from pygame.locals import * # WTF am i doing here, where the fuck is locals
+
 """"
 import numpy # use numpy to model path
 
 """
-player2Spin = [pygame.image.load('L-Drago/LDrago.png'),pygame.image.load('L-Drago/LDrago1.png'),
-               pygame.image.load('L-Drago/LDrago2.png'),pygame.image.load('L-Drago/LDrago3.png'),
-               pygame.image.load('L-Drago/LDrago4.png'),pygame.image.load('L-Drago/LDrago5.png'),
-               pygame.image.load('L-Drago/LDrago6.png'),pygame.image.load('L-Drago/LDrago7.png'),
-               pygame.image.load('L-Drago/LDrago8.png')]
-pygame.init() # isnt inatilizing secondary modules GARBAGE SHIT FACE CODE
 
+# This part is important, creates an array of each bey's images. Cycling through the images makes it spin.
+# Each image is transformed and made smaller to better fit the arena
+player1Spin = [pygame.transform.scale(pygame.image.load('Pegasus/Pegasus1.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus2.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus3.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus4.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus5.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus6.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus7.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus8.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('Pegasus/Pegasus9.png'), (220, 220))]
+
+player2Spin = [pygame.transform.scale(pygame.image.load('LDrago/LDrago1.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago2.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago3.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago4.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago5.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago6.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago7.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago8.png'), (220, 220)),
+               pygame.transform.scale(pygame.image.load('LDrago/LDrago9.png'), (220, 220))]
+
+pygame.init()  # isnt inatilizing secondary modules GARBAGE SHIT FACE CODE
 
 screen = pygame.display.set_mode((1200, 1200))
 pygame.display.set_caption('BeyBlade Game')
 
 # image needs to be in the main file struct cant store in seprate folder, see if u can fix
-#Background obj
+# Background obj
 Background = pygame.image.load('ArenaBeyBladeVroom.jpg').convert()
-screen.blit(Background,(0,0))
+screen.blit(Background, (0, 0))
 
-    #load method can have a specfied path - take that stack overflow u dumb bitch
-    #nvm they were right it cant be path specfic
+# load method can have a specfied path - take that stack overflow u dumb bitch
+# nvm they were right it cant be path specfic
 
-#player obj - Pegasus
-player = pygame.image.load('PegasusIsLame.png')
+# player obj - Pegasus
+player = player1Spin[0]
 player = pygame.transform.scale(player, (300, 300))
-# i think i spelled Pegasus wrong
 position = player.get_rect()
-#make Pegasus smaller its a bit big
-screen.blit(player, (250,250))
+screen.blit(player, (250, 250))
+
 screen.blit(Background, position, position)
 pygame.display.update()
-#https://www.pygame.org/docs/ref/transform.html#pygame.transform.rotate
+# https://www.pygame.org/docs/ref/transform.html#pygame.transform.rotate
 
 # player2 obj - El Drago
-player2 = pygame.image.load('ELDrago1.png')
+player2 = player2Spin[0]
 player2 = pygame.transform.scale(player2, (250, 250))
 # i think i spelled Pegasus wrong
 # nah you didn't
-screen.blit(player2, (650,650))
+screen.blit(player2, (650, 650))
 pygame.display.update()
 clock = pygame.time.Clock()
 i = 0
-speeed = 50
-
+# Speeed is the refresh rate of the game, making it smaller makes it seem as if both beys are going faster. Lowest number
+# is 1 highest number is 80. After 60 it starts to look wonky.
+speeed = 60
+# x is just something I was testing, it's not needed
+x = 2
 done = False
 
 while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-        #screen.blit(player,(500,500)) #blit is temporary and crashes , How to fix?
-                                    # format error screen.blit(object (x,y))
-            # update probaby needs to happen hear in loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    # screen.blit(player,(500,500)) #blit is temporary and crashes , How to fix?
+    # format error screen.blit(object (x,y))
+    # update probaby needs to happen hear in loop
 
-        #pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(500, 500, 100, 100))
-        screen.blit(Background, (0, 0))
-        player = pygame.transform.rotate(player,90)
-        screen.blit(player, (250,250))
-        player2 = pygame.transform.scale(player2Spin[i], (250,250))
-        i+= 3
-        if i >= len(player2Spin):
-                i = 0
-        screen.blit(player2, (550,500))
+    # pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(500, 500, 100, 100))
+    screen.blit(Background, (0, 0))
+    player = player1Spin[i]
+    screen.blit(player, (650 - x, 250))
+    player2 = player2Spin[i]
+    # Again, please ignore x.
+    x += 25
+    # changing i also changes how the beyblades look like when spinning. i = 1 means normal i = 4 means slightly faster
+    # looking spinning. not that useful but thought u should know
+    i += 4
+    i = i % 9
+    screen.blit(player2, (550, 500))
 
-        pygame.display.flip()
-        pygame.time.delay(speeed)
+    pygame.display.flip()
+    pygame.time.delay(speeed)
 # for movement we cant use random values but we can [A,B,C,D] and change the rates at which each grow and decrease to
 # make random movement
