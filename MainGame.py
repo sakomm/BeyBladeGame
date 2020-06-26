2a12047d30177cd976ecf3bab7d27dc152618f84
Anurag kommuri <sakommuri@gmail.com>
2020-06-26 10:05:15 -0400

    Added a timer and screen

--- MainGame.py
+++ MainGame.py
@@ -3,10 +3,11 @@
 """
 import os
 import sys
+import time
 
 # from pygame.locals import * # WTF am i doing here, where the fuck is locals
 import pygame as pg
-
+from pygame.locals import *
 """"
 import numpy # use numpy to model path
 
@@ -16,39 +17,41 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'  # prepriping info gathering
 
 # This part is important, creates an array of each bey's images. Cycling through the images makes it spin.
 # Each image is transformed and made smaller to better fit the arena
-player1Spin = [pg.transform.scale(pg.image.load('Images/Pegasus/Pegasus1.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus2.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus3.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus4.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus5.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus6.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus7.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/Pegasus/Pegasus8.png'), (220, 220)),
-               pg.transform.scale(pg.image.load('Images/Pegasus/Pegasus9.png'), (220, 220))]
+player1Spin = [pg.transform.scale(pg.image.load(
+    'Images/Pegasus/Pegasus1.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus2.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus3.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus4.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus5.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus6.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus7.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/Pegasus/Pegasus8.png'), (220, 220)),
+    pg.transform.scale(pg.image.load('Images/Pegasus/Pegasus9.png'), (220, 220))]
 
-player2Spin = [pg.transform.scale(pg.image.load('Images/LDrago/LDrago1.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago2.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago3.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago4.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago5.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago6.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago7.png'), (220, 220)),
-               pg.transform.scale(pg.image.load(
-                   'Images/LDrago/LDrago8.png'), (220, 220)),
-               pg.transform.scale(pg.image.load('Images/LDrago/LDrago9.png'), (220, 220))]
+player2Spin = [pg.transform.scale(pg.image.load(
+    'Images/LDrago/LDrago1.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago2.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago3.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago4.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago5.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago6.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago7.png'), (220, 220)),
+    pg.transform.scale(pg.image.load(
+        'Images/LDrago/LDrago8.png'), (220, 220)),
+    pg.transform.scale(pg.image.load('Images/LDrago/LDrago9.png'), (220, 220))]
 
 pg.init()  # isnt inatilizing secondary modules GARBAGE SHIT FACE CODE
 
@@ -97,6 +100,37 @@ speeed = 60
 # x is just something I was testing, it's not needed
 x = 2
 done = True
+player1Counter = 0
+player2Counter = 0
+
+# pregame sextion
+timer = 10
+while timer != 0:
+    timer -= 1
+    pg.time.delay(500)
+    for event in pg.event.get():
+        if event.type == KEYDOWN:
+            if event.key == pg.K_q:
+                player1Counter += 1
+
+    for event in pg.event.get():
+        if event.type == KEYDOWN:
+            if event.key == pg.K_KP_DIVIDE:
+                player2Counter += 1
+    
+    if timer == 1:
+        timer = "LET IT RIP"
+    
+    screen.fill((114, 89, 255))
+    myfont = pg.font.SysFont('Robotico', 150)
+
+    textsurface = myfont.render(str(timer), False, (0, 0, 0))
+    screen.blit(textsurface, (Screen_Width//2, Screen_Height//2))
+
+    pg.display.flip()
+
+    print("Player 1 Count:{}, Player 2 Count {}".format(
+        player1Counter, player2Counter))
 
 while done:
 
@@ -117,7 +151,13 @@ while done:
     # looking spinning. not that useful but thought u should know
     i += 4
     i = i % 9
-    screen.blit(player2, (550, 500))    
+    screen.blit(player2, (550, 500))
+
+    for event in pg.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pg.quit()
+                sys.exit()
 
     pg.display.flip()
     pg.time.delay(speeed)
