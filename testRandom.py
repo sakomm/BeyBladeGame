9abcd3ff9f956958f63015f16d0270b389372524
sai <sakommuri@gmail.com>
2020-07-29 20:10:40 -0400

    im scured

--- testRandom.py
+++ testRandom.py
@@ -3,8 +3,8 @@ import random
 for x in range(0,1000,1):
     x = random.randrange(0,1000,1)
 
-    print(x,end = "//")
-    if x > 900:
+    #print(x,end = "//")
+    if x > 800:
         print("[poop]")
     else:
         print("-",end = "")
